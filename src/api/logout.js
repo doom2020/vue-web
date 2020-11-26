5e7b13798e5b1dd1cc39cdda362c46651047810c
yuanzhijian <v-yuanzhijian@xiaomi.com>
2020-11-26 21:28:13 +0800

    退出登录api完成

---
+++ src/api/logout.js
@@ -0,0 +1,14 @@
+import request from '../utils/request'
+
+
+export function logout(params){
+    console.log(params)
+    return request({
+        url: '/',
+        method: 'post',
+        data: {
+            post_type: 'logout'
+        },
+        responseType: 'json'
+    })
+}
