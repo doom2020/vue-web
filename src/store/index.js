56de38ae988367dfcd025d6032ce7f0024cd6c55
yuanzhijian <v-yuanzhijian@xiaomi.com>
2020-11-27 10:42:16 +0800

    添加一个简单的状态管理用于测试

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,16 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    count: 0,
+    name: 'zhu'
   },
   mutations: {
+    add(state,paylod){
+      state.count += paylod
+    },
+    update(state, paylod){
+      state.name += paylod
+    }
   },
   actions: {
   },
